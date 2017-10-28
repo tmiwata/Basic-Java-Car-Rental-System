179d00d2975379d590c258cb6f64fc08fc9ea1c7
tmiwata <takayukimiwata@gmail.com>
2017-10-29 00:12:03 +0900

    Rental.java
    Fixed Indentation

--- Romy Car Rental/src/Rental.java
+++ Romy Car Rental/src/Rental.java
@@ -4,19 +4,19 @@ import java.awt.event.*;
 
 public class Rental{ 
 
-	private static final int WIDTH = 600;
+  private static final int WIDTH = 600;
   private static final int HEIGHT = 400;
 
 //////////////////////////////////////////////////////////MAIN//////////////////////////////////////////
-   public static void main(String[] args) {
-   
-		JFrame frame = new RentalView();
-		ImageIcon img = new ImageIcon("../images/logo.GIF");
-		frame.setIconImage(img.getImage());
+  public static void main(String[] args) {
+
+    JFrame frame = new RentalView();
+    ImageIcon img = new ImageIcon("../images/logo.GIF");
+    frame.setIconImage(img.getImage());
     frame.setTitle("Romy's Car Rental v.01");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(WIDTH, HEIGHT);
-		frame.setVisible(true);
+    frame.setVisible(true);
     frame.setResizable(false);
-   }	 
+   }
 }
